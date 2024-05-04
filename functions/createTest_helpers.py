f40764d9387f78fe39d0cfe0fb712159d1cb0ec1
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2024-05-04 17:13:07 -0400

    added initial mostly-working createTest fxn

--- functions/createTest_helpers.py
+++ functions/createTest_helpers.py
@@ -11,20 +11,23 @@ test_list = [
     "DataLog_2024-0430-2328-01_CMS_Data_Wiring_6.tdms",
     "DataLog_2024-0501-0002-02_CMS_Data_Wiring_6.tdms",
     "DataLog_2024-0501-0031-41_CMS_Data_Wiring_6.tdms",
+    "timestamped_bangbang_data.csv",
 ]
 
-csv_test_list = ["timestamped_bangbang_data"]
+csv_test_list = ["timestamped_bangbang_data.csv"]
 
 
-def organizeTDMSFiles(file_names: list[str]):
-    file_names.sort()
+def organizeFiles(file_names: list[str]):
+    csv_files = list(filter(lambda x: ".csv" in x, file_names))
+    fileNames = list(filter(lambda x: ".csv" not in x, file_names))
+    fileNames.sort()
     timestamps: list[int] = []
-    for file in file_names:
+    for file in fileNames:
         time_stamp_str = file[8:25]
         datetimeObj = datetime.strptime(time_stamp_str, "%Y-%m%d-%H%M-%S")
         dateString = time.mktime(datetimeObj.timetuple())
         timestamps.append(int(dateString))
-    return (file_names, timestamps)
+    return (fileNames, csv_files, timestamps)
 
 
 def downloadFromGDrive(url: str):
@@ -32,10 +35,5 @@ def downloadFromGDrive(url: str):
     file_name = gdown.download(url=url, fuzzy=True)
     return file_name
 
-def parseCSV(filename: str):
-    # stuffs = daq.parseCSV(0, file_path_custom="C:/Users/rajan/Desktop/psp-platform/functions/test_data/timestamped_bangbang_data.csv")
-    output: dict[str, SensorNetData] = parseCSV(file_path_custom=filename)
-    print("done")
 
-
-parseCSV()
+# organizeFiles(test_list)
--- functions/main.py
+++ functions/main.py
@@ -2,24 +2,37 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
+import math
 from multiprocessing import cpu_count
 from multiprocessing.pool import ThreadPool
 import os
+import pickle
 from typing import Any
 from firebase_functions import https_fn, options
 from firebase_admin import initialize_app, App
 import base64
 import datetime
 from google.cloud import storage
+import google.cloud.firestore as fs
 from google.oauth2 import service_account
 import firebase_admin
 from firebase_admin import credentials, firestore, App
 import pandas as pd
-from psp_liquids_daq_parser import parseCSV, parseTDMS, AnalogChannelData, DigitalChannelData, SensorNetData, extendDatasets
+from psp_liquids_daq_parser import (
+    parseCSV,
+    parseTDMS,
+    AnalogChannelData,
+    DigitalChannelData,
+    SensorNetData,
+    extendDatasets,
+    combineTDMSDatasets,
+)
 from google.cloud.storage import Client, transfer_manager
 
-from functions.createTest_helpers import downloadFromGDrive, organizeTDMSFiles
+from createTest_helpers import downloadFromGDrive, organizeFiles
+
 app: App = initialize_app()
+
 
 @https_fn.on_call(secrets=["GOOGLE_ADC"])
 def createCSV(req: https_fn.CallableRequest) -> Any:
@@ -36,11 +49,11 @@ def createCSV(req: https_fn.CallableRequest) -> Any:
     #         "csv_fields": decoded_string,
     #         "name": test_name
     #     }
-    bucket_name = 'psp-data-viewer-storage'
-    blob_folder_name = 'cms_whoopsie'
+    bucket_name = "psp-data-viewer-storage"
+    blob_folder_name = "cms_whoopsie"
     blob_name = "DataLog_2024-0430-2328-01_CMS_Data_Wiring_6.tdms"
     with open("adc.json", "w") as file1:
-        file1.write(os.environ.get('GOOGLE_ADC'))
+        file1.write(os.environ.get("GOOGLE_ADC"))
     credentials = service_account.Credentials.from_service_account_file("adc.json")
     storage_client = storage.Client(credentials=credentials)
     bucket = storage_client.bucket(bucket_name)
@@ -53,7 +66,8 @@ def createCSV(req: https_fn.CallableRequest) -> Any:
     print("Generated GET signed URL:")
     return url
 
-@https_fn.on_call(timeout_sec=360, memory=options.MemoryOption.GB_2)
+
+@https_fn.on_call(timeout_sec=540, memory=options.MemoryOption.GB_2)
 def createTest(req: https_fn.CallableRequest) -> Any:
     data = req.data
     test_name: str = data["test_name"]
@@ -62,39 +76,42 @@ def createTest(req: https_fn.CallableRequest) -> Any:
     gse_article: str = data["gse_article"]
     url_pairs: list[str] = data["url_pairs"]
     file_names: list[str] = []
-    
+
     print("downloading...")
     cpus = cpu_count()
     results = ThreadPool(cpus - 1).imap_unordered(downloadFromGDrive, url_pairs)
     for result in results:
         file_names.append(result)
-        print('downloaded:', result)
-
-    (file_names, starting_timestamps) = organizeTDMSFiles(file_names)
+        print("downloaded:", result)
+    (tdms_filenames, csv_filenames, starting_timestamps) = organizeFiles(file_names)
     parsed_datasets: dict[
         str,
         AnalogChannelData | DigitalChannelData | SensorNetData | list[float],
     ] = []
-    parsed_datasets.update(parseTDMS(0,starting_timestamps[-1], file_path_custom=file_names[-1]))
-    parsed_datasets.update(parseTDMS(0,starting_timestamps[-2], file_path_custom=file_names[-2]))
-
-    [channels, df_const] = extendDatasets(parsed_datasets)
+    file1 = parseTDMS(0, file_path_custom=tdms_filenames[-1])
+    file2 = parseTDMS(0, file_path_custom=tdms_filenames[-2])
+    parsed_datasets = combineTDMSDatasets(file1, file2)
+    parsed_datasets.update(parseCSV(file_path_custom=csv_filenames[-1]))
+    [channels, max_length, data_as_dict] = extendDatasets(parsed_datasets)
+    print("pickling data...")
+    with open("test_data/all_channels.pickle", "wb") as f:
+        pickle.dump(data_as_dict, f, pickle.HIGHEST_PROTOCOL)
+    print("pickled data")
+    file_names.append("test_data/all_channels.pickle")
 
     db = firestore.client()
     dict_to_write: dict[str, list[float]] = {}
-    all_time: list[float] = df_const["time"]
+    all_time: list[float] = data_as_dict["time"]
     available_datasets: list[str] = []
-    for dataset in df_const:
+    for dataset in data_as_dict:
         if dataset != "time":
-            data: list[float] = df_const[dataset]
-            # time: list[float] = parsed_datasets[dataset].time.tolist()
+            data: list[float] = data_as_dict[dataset]
             time: list[float] = all_time[: len(data)]
             df = pd.DataFrame.from_dict({"time": time, "data": data})
             # df_cut = df.head(15*1000)
-            thing = df.iloc[::500, :]
+            thing = df.iloc[:: math.ceil(max_length / 4000), :]
             # print("writing csv...")
             # df.to_csv(dataset+".csv", lineterminator="\n",index=False)
-
 
             scale = "psi"
             if "tc" in dataset:
@@ -105,7 +122,7 @@ def createTest(req: https_fn.CallableRequest) -> Any:
                 scale = "lbf"
             if "rtd" in dataset:
                 scale = "V"
-            doc_ref = db.collection(test_name).document(dataset)
+            doc_ref = db.collection(test_id).document(dataset)
             doc_ref.set({"time_offset": (time[0])})
             doc_ref.set(
                 {
@@ -118,15 +135,41 @@ def createTest(req: https_fn.CallableRequest) -> Any:
             available_datasets.append(dataset)
             print(dataset)
 
-    doc_ref = db.collection(test_name).document("general")
-    doc_ref.set({"datasets": available_datasets, "test_article": "CMS", "gse_article":"BCLS", "name": test_name}, merge=True)
-
+    doc_ref_test_general = db.collection(test_id).document("general")
+    doc_ref_test_general.set(
+        {
+            "datasets": available_datasets,
+            "test_article": test_article,
+            "gse_article": gse_article,
+            "name": test_name,
+        },
+        merge=True,
+    )
+    general_doc_ref = db.collection("general").document("tests")
+    general_doc_ref.update(
+        {
+            "visible": fs.ArrayUnion(
+                [
+                    {
+                        "id": test_id,
+                        "test_article": test_article,
+                        "gse_article": gse_article,
+                        "name": test_name,
+                    }
+                ]
+            )
+        }
+    )
 
     storage_client = storage.Client()
     bucket = storage_client.bucket("psp-data-viewer-storage")
     print("uploading...")
     results = transfer_manager.upload_many_from_filenames(
-        bucket, file_names, blob_name_prefix=test_id + "/", source_directory=".", max_workers=6
+        bucket,
+        file_names,
+        blob_name_prefix=test_id + "/",
+        source_directory=".",
+        max_workers=6,
     )
     for name, result in zip(file_names, results):
         # The results list is either `None` or an exception for each filename in
@@ -135,5 +178,4 @@ def createTest(req: https_fn.CallableRequest) -> Any:
             print("Failed to upload {} due to exception: {}".format(name, result))
         else:
             print("Uploaded {} to {}.".format(name, bucket.name))
-    return {"name": test_name, "first_url": url_pairs[0]}
-
+    return {"name": test_name, "id": test_id}
