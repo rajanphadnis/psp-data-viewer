211e33dd05a9d9066e743d67c207d03913cbc162
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2024-05-04 20:23:55 -0400

    added intial get csv framework

--- functions/main.py
+++ functions/main.py
@@ -38,34 +38,63 @@ app: App = initialize_app()
 def createCSV(req: https_fn.CallableRequest) -> Any:
     data = req.data
     base64_data: str = data["b64"]
-    test_name: str = data["test_name"]
-    # if (base64_data == ""):
-    #     print("exporting all")
-    #     return False
-    # else:
-    #     decoded_string = base64.b64decode(str(base64_data)).decode("utf-8")
-    #     list_of_channels: list[str] = decoded_string.split(",")
-    #     print(decoded_string)
-    #     return {
-    #         "csv_fields": decoded_string,
-    #         "name": test_name
-    #     }
+    test_id: str = data["test_id"]
     bucket_name = "psp-data-viewer-storage"
-    blob_folder_name = "cms_whoopsie"
-    blob_name = "DataLog_2024-0430-2328-01_CMS_Data_Wiring_6.tdms"
+    blob_folder_name = test_id + "/temp"
+    blob_name = "all_channels.csv"
     with open("adc.json", "w") as file1:
         file1.write(os.environ.get("GOOGLE_ADC"))
     credentials = service_account.Credentials.from_service_account_file("adc.json")
     storage_client = storage.Client(credentials=credentials)
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(blob_folder_name + "/" + blob_name)
-    url = blob.generate_signed_url(
-        version="v4",
-        expiration=datetime.timedelta(minutes=60),
-        method="GET",
+    all_channels_pickle_blob = bucket.blob(test_id + "/test_data/all_channels.pickle")
+    all_channels_pickle_blob.download_to_filename("all_channels.pickle")
+    print("unpickling expanded datasets...")
+    with open("all_channels.pickle", "rb") as f:
+                data_datasets = pickle.loads(f.read())
+    print("unpickled expanded datasets")
+    print("expanding channels...")
+    df = pd.DataFrame.from_dict(data_datasets)
+    if (base64_data == ""):
+        print("exporting all")
+        df.to_csv(blob_name)
+        print("export complete")
+        # return False
+    else:
+        decoded_string = base64.b64decode(str(base64_data)).decode("utf-8")
+        list_of_channels: list[str] = decoded_string.split(",")
+        blob_name = ("_".join(list_of_channels)) + ".csv"
+        print("exporting datasets")
+        df[list_of_channels].to_csv(blob_name)
+        print("export complete")
+        # return {
+        #     "csv_fields": decoded_string,
+        #     "name": test_name
+        # }
+    print("uploading...")
+    results = transfer_manager.upload_many_from_filenames(
+        bucket,
+        [blob_name],
+        blob_name_prefix=blob_folder_name + "/",
+        source_directory=".",
+        max_workers=2,
     )
-    print("Generated GET signed URL:")
-    return url
+    for name, result in zip([blob_name], results):
+        # The results list is either `None` or an exception for each filename in
+        # the input list, in order.
+        if isinstance(result, Exception):
+            print("Failed to upload {} due to exception: {}".format(name, result))
+            return False
+        else:
+            print("Uploaded {} to {}.".format(name, bucket.name))
+            blob = bucket.blob(blob_folder_name + "/" + blob_name)
+            url = blob.generate_signed_url(
+                version="v4",
+                expiration=datetime.timedelta(minutes=60),
+                method="GET",
+            )
+            print("Generated GET signed URL:")
+            return url
 
 
 @https_fn.on_call(timeout_sec=540, memory=options.MemoryOption.GB_2)
