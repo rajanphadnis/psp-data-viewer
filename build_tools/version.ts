ba37312833860b98067ad23063651818da06da95
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2024-09-03 19:38:30 -0400

    bug fixes and bump to v0.2.15

--- build_tools/version.ts
+++ build_tools/version.ts
@@ -1,6 +1,8 @@
 import { generateChangelog, getCurrentChangelogVersion, writeChangelog, writeNewPackageVersion } from "./file_fxns";
 import { ChangeType } from "./types";
 import minimist from "minimist";
+import { exec } from "child_process";
+import open from "open";
 
 const patchType_cmdline: string = minimist(process.argv.slice(2))["_"][0] ?? "patch";
 const app_names: string[] = ["admin", "docs", "webapp"];
@@ -36,13 +38,32 @@ getCurrentChangelogVersion()
       app_names.forEach((app_name) => {
         const current_version: string = require(`../${app_name}/package.json`).version;
         const newVersion = incrementVersion(current_version, changeType);
-        writeNewPackageVersion(app_name, newVersion);
+        // writeNewPackageVersion(app_name, newVersion);
       });
       const newVersion = incrementVersion(changelog_version, changeType);
       const newChangelog = await generateChangelog(newVersion, changeType);
-      writeChangelog(newChangelog);
+      // writeChangelog(newChangelog);
       console.log("files updated successfully");
-      console.log(`Click to start release process: https://github.com/rajanphadnis/psp-data-viewer/releases/new?tag=v${newVersion}`);
+      if (changeType == ChangeType.PATCH) {
+        const response = await new Promise<void>(async (resolve, reject) => {
+          exec(
+            `git add . && git commit -m "bug fixes and bump to v${newVersion}" && git push origin main`,
+            (err, stdout, stderr) => {
+              // your callback
+              console.log(stdout);
+              resolve();
+            }
+          );
+        });
+        console.log(
+          `Click to start release process: https://github.com/rajanphadnis/psp-data-viewer/releases/new?tag=v${newVersion}`
+        );
+        open(`https://github.com/rajanphadnis/psp-data-viewer/releases/new?tag=v${newVersion}`);
+      } else {
+        console.log(
+          `Once you've updated the changelog and pushed to git, click to start release process: https://github.com/rajanphadnis/psp-data-viewer/releases/new?tag=v${newVersion}`
+        );
+      }
     } else if (package_versions_identical) {
       console.error("Changelog version doesn't match package.json versions!");
     } else {
