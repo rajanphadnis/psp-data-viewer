bb6b1ed84258990286665587522cd59cc771f214
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2024-09-21 01:58:51 -0400

    added delete fxn

--- functions/main.py
+++ functions/main.py
@@ -519,3 +519,104 @@ def getAPIConfig(req: https_fn.Request) -> https_fn.Response:
         return https_fn.Response(json.dumps({"status": "Success", "result": cli.result.result}), status=200)
     else:
         return https_fn.Response(json.dumps({"status": f"Failed, exit code: {thing}", "result": cli.result.error}), status=500)
+
+@https_fn.on_request(
+    cors=options.CorsOptions(
+        cors_origins="*",
+        cors_methods=["get", "post"],
+    ),
+    memory=options.MemoryOption.MB_512,
+)
+def deleteTest(req: https_fn.Request) -> https_fn.Response:
+    # Comments
+    id = req.args["id"] if "id" in req.args else None
+    if id is None:
+        return https_fn.Response(
+            json.dumps({"status": "'id' is required"}), status=400
+        )
+
+    appId = os.environ.get("AZURE_APP_ID")
+    password = os.environ.get("AZURE_PASSWORD_STRING")
+    tenant = os.environ.get("AZURE_TENANT_STRING")
+
+    print(appId)
+    print(password)
+    print(tenant)
+
+    db: google.cloud.firestore.Client = firestore.client()
+    transaction: google.cloud.firestore.Transaction = db.transaction()
+    test_ref: google.cloud.firestore.CollectionReference = db.collection(
+        id
+    )
+    general_ref: google.cloud.firestore.DocumentReference = db.collection(
+        "general"
+    ).document("tests")
+
+    bucket = storage.bucket()
+    blobs = bucket.list_blobs(prefix=f"{id}/")
+    
+    for blob in blobs:
+        blob.delete()
+        print(f"Deleted {blob.name}")
+
+
+    @firestore.transactional
+    def update_in_transaction(
+        transaction: google.cloud.firestore.Transaction,
+        general_ref: google.cloud.firestore.DocumentReference,
+        test_ref: google.cloud.firestore.CollectionReference,
+    ):
+        list_of_visible: list = general_ref.get(transaction=transaction).get("visible")
+        print(list_of_visible)
+        item_to_index = [item for item in list_of_visible if item["id"] == id]
+        print(item_to_index)
+        transaction.update(
+            general_ref, {"visible": google.cloud.firestore.ArrayRemove(item_to_index)}
+        )
+        transaction.delete(
+            test_ref.document("general"),
+        )
+        transaction.delete(
+            test_ref.document("ready_to_deploy"),
+        )
+        transaction.delete(
+            test_ref.document("test_creation"),
+        )
+        return True
+
+    result = update_in_transaction(transaction, general_ref, test_ref)
+
+    try:
+        thing = get_default_cli().invoke(
+            [
+                "login",
+                "--service-principal",
+                "-u",
+                appId,
+                "-p",
+                password,
+                "--tenant",
+                tenant,
+            ]
+        )
+        # thing = get_default_cli().invoke(["webapp", "config", "storage-account", "list", "--resource-group", "pspdataviewer", "--name", "psp-data-viewer-api"])
+        thing = get_default_cli().invoke(
+            [
+                "storage",
+                "file",
+                "delete",
+                "--path",
+                f"hdf5_data/{id}.hdf5",
+                "--share-name",
+                "psp-data-viewer3d1877",
+                "--account-name",
+                "pspdataviewer",
+            ]
+        )
+
+    except Exception as e:
+        return https_fn.Response(json.dumps({"status": f"error: {e}"}), status=500)
+    if (thing == 0):
+        return https_fn.Response(json.dumps({"status": "Success"}), status=200)
+    else:
+        return https_fn.Response(json.dumps({"status": f"Failed, exit code: {thing}"}), status=500)
