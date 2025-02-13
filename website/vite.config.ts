9409040ac03300868ddee477c7955dd585118084
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2025-02-13 14:42:38 -0800

    updated to public file

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
+// import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [tailwindcss(), solidPlugin()],
+  plugins: [
+    tailwindcss(),
+    solidPlugin(),
+    // viteStaticCopy({
+    //   targets: [
+    //     {
+    //       src: "bin/example.wasm",
+    //       dest: "wasm-files",
+    //     },
+    //   ],
+    // }),
+  ],
   server: {
     port: 3000,
   },
