93a07935ce75e856966c3ff50c947bc7e0bf7fde
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2025-02-13 12:25:40 -0800

    added links

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,7 +1,13 @@
 import type { Component } from "solid-js";
 
 const App: Component = () => {
-  return <div>Under Construction</div>;
+  return (
+    <div>
+      Under Construction
+      <a href="./azcompiled.zip" download>Azure Compiled Zip</a>
+      <a href="./dataviewer.exe" download>Dataviewer.exe</a>
+    </div>
+  );
 };
 
 export default App;
