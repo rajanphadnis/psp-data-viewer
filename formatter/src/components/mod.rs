c14353a6bfde7a1f88d6c067537353a18d9c4f9b
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2025-02-26 21:37:38 -0800

    completed transition to tauri-based desktop app

--- formatter/src/components/mod.rs
+++
@@ -1,6 +0,0 @@
-mod file_list;
-mod sidebar;
-mod processing_button;
-pub use file_list::FileList;
-pub use sidebar::SideBar;
-pub use processing_button::ProcessingButton;
--- formatter/src/processing/mod.rs
+++
@@ -1,3 +0,0 @@
-mod create_df;
-mod parse_types;
-pub use create_df::create;
